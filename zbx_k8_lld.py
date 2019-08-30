253680b6a625a19ba8c41e74e6ecc5078dde3ff8
Ronald Rood <ronr@ronr.nl>
2019-08-30 13:13:35 +0200

    some err handling

--- zbx_k8_lld.py
+++ zbx_k8_lld.py
@@ -2,34 +2,41 @@
 # should work with python 2.7 and 3
 import json
 import subprocess
+import sys
 
-process = subprocess.Popen(["kubectl get pods -o json"],
+data = ""
+process = subprocess.Popen(["kubectlsss get pods -o json"],
                            shell=True,
                            stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE,
                            close_fds=True)
-data = json.loads(process.stdout.read().decode())
-err = process.stderr.read().decode()
 exit_code = process.wait()
+
+if exit_code == 0:
+    data = json.loads(process.stdout.read().decode())
+err = process.stderr.read().decode()
 # with open("get_pods.json","r") as readfile:
     # data = json.load(readfile)
 
 containers = []
 # print(data)
 
-for i in data["items"]:
-    # print("{} {} {:>5} {}".format(
-        # i["kind"],
-        # i["metadata"]["namespace"],
-        # i["metadata"]["resourceVersion"],
-        # i["metadata"]["name"]
-        # ))
+if data:
+    for i in data["items"]:
+        # print("{} {} {:>5} {}".format(
+            # i["kind"],
+            # i["metadata"]["namespace"],
+            # i["metadata"]["resourceVersion"],
+            # i["metadata"]["name"]
+            # ))
 
-    for c in i["spec"]["containers"]:
-        e = {"{#NAME}": i["metadata"]["name"], "{#NAMESPACE}":
-                i["metadata"]["namespace"], "{#CONTAINER}": c["name"]}
-        # print("    {} {}".format(c["name"],c["image"]))
-        containers.append(e)
+        for c in i["spec"]["containers"]:
+            e = {"{#NAME}": i["metadata"]["name"], "{#NAMESPACE}":
+                    i["metadata"]["namespace"], "{#CONTAINER}": c["name"]}
+            # print("    {} {}".format(c["name"],c["image"]))
+            containers.append(e)
 
 # print(containers)
-print("{\"data\":"+json.dumps(containers)+"}")
+    print("{\"data\":"+json.dumps(containers)+"}")
+else:
+    print(err, file=sys.stderr)
