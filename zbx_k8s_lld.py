7c2e7809ff74ed610e97915c2c4692be57b7f2cf
Ronald Rood <ronr@ronr.nl>
2019-09-09 10:20:54 +0200

    added node

--- zbx_k8s_lld.py
+++ zbx_k8s_lld.py
@@ -1,31 +1,40 @@
 #!/usr/bin/env python
 # should work with python 2.7 and 3
+from __future__ import print_function
 import json
+import platform
 import subprocess
 import sys
 
 data = ""
-process = subprocess.Popen(["kubectl get pods -o json"],
+cmd = "kubectl get pod -o=custom-columns=NAME:.metadata.name,STATUS:.status.phase,NODE:.spec.nodeName  --all-namespaces -o json"
+
+if int(platform.python_version().split('.')[0]) < 3:
+    msg = cmd.decode()
+else:
+    msg = cmd
+process = subprocess.Popen( [msg],
                            shell=True,
                            stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE,
                            close_fds=True)
+(out, err) = process.communicate()
 exit_code = process.wait()
 
 if exit_code == 0:
-    data = json.loads(process.stdout.read().decode())
-err = process.stderr.read().decode()
-# with open("get_pods.json","r") as readfile:
-    # data = json.load(readfile)
+    data = json.loads(out)
+    # data = json.loads(process.stdout.read().decode())
 
 containers = []
 
 if data:
+
     for i in data["items"]:
 
         for c in i["spec"]["containers"]:
             e = {"{#NAME}": i["metadata"]["name"], "{#NAMESPACE}":
-                    i["metadata"]["namespace"], "{#CONTAINER}": c["name"]}
+                    i["metadata"]["namespace"], "{#CONTAINER}": c["name"],
+                    "{#NODE}": i["spec"]["nodeName"]}
             containers.append(e)
 
     print("{\"data\":"+json.dumps(containers)+"}")
